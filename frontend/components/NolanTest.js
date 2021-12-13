8fa9d12f83d2437c6857c607fe039673a9eac17b
Pornchai Chanaloet <kayziopro@gmail.com>
2021-12-13 19:20:26 +0100

    change to web version

--- frontend/components/NolanTest.js
+++ frontend/components/NolanTest.js
@@ -8,12 +8,12 @@ const NolanTest = () => {
             <div className={styles.container}>
                 <div className={styles.navbar}>
                     <img style={{marginTop:'3px'}} width="400px" height="auto" src="https://telephonieip.standardenligne.fr/static/media/logo.9db1994e.svg" alt="logo-profil"/>
-                    <i style={{fontSize: '70px',fontWeight: 'bold', float: 'right', marginRight: '30px', paddingTop: '10px', cursor: 'pointer', color: '#00468b',}} class="far fa-bars"></i>
+                    <i style={{fontSize: '70px',fontWeight: 'bold', float: 'right', marginRight: '30px', paddingTop: '10px', cursor: 'pointer', color: '#00468b', position: 'static'}} class="far fa-bars"></i>
                 </div>
             
             
             <div className={styles.textcontainer1}>
-                <p className={styles.font1}>Informations du forfait</p>
+                <p className={styles.font1}>Informations du Forfait</p>
             </div>
 
             <div>
@@ -24,7 +24,7 @@ const NolanTest = () => {
         <div className={styles.steps}>
 
                 <div className={styles.stepSelected} >1
-                <img src="https://i.ibb.co/B31CDDn/1024px-Eo-circle-grey-checkmark-svg-1.png" style={{weight: '65%', height: '65%', zIndex:'3', color:'#797979', paddingLeft: '20px', position: 'relative', align: 'center', display: 'flex', justifyContent: 'center', bottom: '5px', color: 'white', backgroundColor: 'transparent'}} alt="icon"></img>
+                <img src="https://i.ibb.co/qjkW82N/innercircle.png" style={{weight: '90%', height: '90%', zIndex:'3', color:'#797979', paddingLeft: '20px', position: 'relative', align: 'center', display: 'flex', justifyContent: 'center', color: 'white', backgroundColor: 'transparent', marginLeft: '8px', bottom: '25px'}} alt="icon"></img>
                 </div>
 
                 <div className={styles.step} >2</div>
@@ -45,29 +45,29 @@ const NolanTest = () => {
     </div>
 
 
+
             <div className={styles.items}>
                 <div className={styles.forfait1}>Forfait 10/mois <br/>de réception</div>
                 <div className={styles.forfait2}>Sur vos fixes et mobiles</div>
                 <div className={styles.forfait3}>14.90€ HT</div>
             </div>
-            </div>
+        </div>
 </div>        
 
-        <div>
-            <div className={styles.separator1}>
-            </div>
-        </div>
-
         <div className={styles.numberContainer}>
+                <div className={styles.separator1}>
             <div className={styles.numbertype}>Type de numéro :</div>
             <div className={styles.number}>Numéro Argent : 01 87 200 200</div>
             <div className={styles.region}>Zone :</div>
             <div className={styles.city}>Paris - 01</div>
-        </div>
+            </div>
 
-        <div className={styles.buttonContainer}>
+            <div className={styles.buttonContainer}>
             <button className={styles.button}>SUIVANT</button>
         </div>
+        
+        </div>
+
 
         <div className={styles.bottomContainer}></div>
 
